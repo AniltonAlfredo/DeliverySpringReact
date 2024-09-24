394c366df2a31e1d51f0f67159d180f866dcbe55
AniltonAlfredo <aniltonengenheiro@gmail.com>
2024-09-24 19:35:36 +0100

    orders put

--- Backend/src/main/java/com/atma/atmadeliver/controllers/OrderController.java
+++ Backend/src/main/java/com/atma/atmadeliver/controllers/OrderController.java
@@ -1,5 +1,6 @@
 package com.atma.atmadeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.atma.atmadeliver.dto.OrderDTO;
 import com.atma.atmadeliver.services.OrderService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 @RequestMapping("/orders")
@@ -22,4 +30,20 @@ public class OrderController {
         List<OrderDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+    
+    @PutMapping("/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+        OrderDTO dto = service.setDelivered(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+
+
 }
--- Backend/src/main/java/com/atma/atmadeliver/services/OrderService.java
+++ Backend/src/main/java/com/atma/atmadeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.atma.atmadeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.atma.atmadeliver.repositories.OrderRepository;
+import com.atma.atmadeliver.repositories.ProductRepository;
 import com.atma.atmadeliver.dto.OrderDTO;
+import com.atma.atmadeliver.dto.ProductDTO;
 import com.atma.atmadeliver.entidades.Order;
+import com.atma.atmadeliver.entidades.OrderStatus;
+import com.atma.atmadeliver.entidades.Product;
 
 
 @Service
@@ -18,12 +23,37 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Transactional(readOnly = true)
-    public List<OrderDTO
-    > findAll(){
+    public List<OrderDTO> 
+    findAll(){
         // List<Order> list = repository.findAll();
         List<Order> list = repository.findOrdersWithProducts();
         return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
     }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto){
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING );
+
+        for (ProductDTO p : dto.getProducts()){
+            Product product = productRepository.getReferenceById(p.getId());
+            order.getProducts().add(product);
+        }
+
+        order = repository.save(order);
+        return new OrderDTO(order);
+    } 
     
+    @Transactional
+    public OrderDTO setDelivered(Long id){
+        Order order = repository.getReferenceById(id);
+        order.setStatus(OrderStatus.DELIVERED);
+        order = repository.save(order);
+        return new OrderDTO(order);
+    }
+
+
 }
