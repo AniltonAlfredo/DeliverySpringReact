9efe9ab84f65747a28a88a455aa5e0945309ac09
AniltonAlfredo <aniltonengenheiro@gmail.com>
2024-09-24 15:42:14 +0100

    projecto criado

---
+++ Backend/src/main/java/com/atma/atmadeliver/AtmadeliverApplication.java
@@ -0,0 +1,13 @@
+package com.atma.atmadeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmadeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmadeliverApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/atma/atmadeliver/AtmadeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atma.atmadeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmadeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
